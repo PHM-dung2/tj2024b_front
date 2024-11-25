77caf2b448c43e846e5a67f7247a947a42eed143
PHM-dung2 <pp4992@naver.com>
2024-11-25 17:15:08 +0900

    종합과제 테두리 수정

---
+++ day11/2_중간과제.js
@@ -0,0 +1,15 @@
+let 배열1 = [ * , * , * ]
+let 배열2 = [ * , * , * ]
+let 배열3 = [ * , * , * ]
+
+let a1 = prompt( "1열 1칸" )
+let a2 = prompt( "1열 2칸" )
+let a3 = prompt( "1열 3칸" )
+let b1 = prompt( "2열 1칸" )
+let b2 = prompt( "2열 2칸" )
+let b3 = prompt( "2열 3칸" )
+let c1 = prompt( "3열 1칸" )
+let c2 = prompt( "3열 2칸" )
+let c3 = prompt( "3열 3칸" )
+
+let 배열놓기 = 배열1.indexOf( `배열1`)
