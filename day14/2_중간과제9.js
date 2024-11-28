2c60bb0f3f04c64f1b01c05aa60594cd3619356f
PHM-dung2 <pp4992@naver.com>
2024-11-29 06:04:52 +0900

    중간과제 입차함수 정의

--- day14/2_중간과제9.js
+++ day14/2_중간과제9.js
@@ -1,22 +1,38 @@
 let carArray = [ '' , '' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'']
-let index = Number(document.querySelector('.carWhat'))
 
-function inFunc( index ){ 
-    console.log('입차함수 실행');
+function inFunc( ){ 
+    // console.log('입차함수 실행');
  
-    let Number = document.querySelector('.carNumber').value;    // console.log(Number);
-    let What = Number(document.querySelector('.carWhat').value);        // console.log(What);
- 
-    //  carArray = Number;
-    //  carArray = What;
+    let Number = document.querySelector('.carNumber').value;
+    let carNum = document.querySelector('.carNum').value;
+    // Uncaught ReferenceError: what is not defined => 변수명 바꿔보기
+    // console.log(Number);
+    // console.log(What);
+    let i = parseInt(carNum);
+    console.log(i);
     let nowDate = new Date();
     let nowHours = nowDate.getHours();
     let nowMinutes = nowDate.getMinutes();
     let nowSeconds = nowDate.getSeconds();
     let inDay = (3600 * nowHours) + (60 * nowMinutes) + nowSeconds;
 
-    carArray[index+1] = `${Number},${What},${inDay}`;
+    let board = carArray[i-1]
+    let carBoard = board.split(',')
+
+    if( carBoard[1] == i){ alert('입차불가'); }
+    else( carArray[i-1] = `${Number},${carNum},${inDay}` )
+       
     console.log(carArray);
+
+    let html =''
+    for( j = 0; j < 20 ; j++){
+        if( (j+1)%5 == 1 ){ html += `<tr>` }
+        if( i == j+1 ){ html += `<td>${Number}</td>` }
+        else{ html += `<td>${j+1}</td>` }
+        if( (j+1)%5 == 0 ){ html += `</tr>` }
+
+    } // for end
+    document.querySelector('.carPosition').innerHTML = html
 
 
     //  carArray.push(board);
@@ -25,28 +41,28 @@ function inFunc( index ){
  
 
 function outFunc( ){
-    let carNum = document.querySelector('.outNum')
-    let i = carArray.indexOf( carNum )
-    console.log(i)
+    let carNum = document.querySelector('.outNum').value;
+    let i = carArray.indexOf( carNum );
+    console.log(i);
     // let board = carArray() 
 }
 
 
 
-let nowDate = new Date();
-let nowHours = Number(nowDate.getHours());
-let nowminutes = Number(nowDate.getMinutes());
-let nowSeconds = Number(nowDate.getSeconds());
-let inDay = (3600 * nowHours) + (60 * nowminutes) + nowSeconds;
 
-function parking( ){
-    let board = carArray[0];
-    let inCar = board.split(",");
-    let price = (inDay - inCar[2])*100;
-    console.log( price );
-    alert( `${inCar[0]} 주차요금은 ${price}원입니다.`)
+// function parking( ){
+//     let nowDate = new Date();
+//     let nowHours = Number(nowDate.getHours());
+//     let nowminutes = Number(nowDate.getMinutes());
+//     let nowSeconds = Number(nowDate.getSeconds());
+//     let inDay = (3600 * nowHours) + (60 * nowminutes) + nowSeconds;
+//     let board = carArray[0];
+//     let inCar = board.split(",");
+//     let price = (inDay - inCar[2])*100;
+//     console.log( price );
+//     alert( `${inCar[0]} 주차요금은 ${price}원입니다.`);
 
-}
+// }
 
 
 
