c5eca2bcf91e4bdd341debae69769661aaaeb7cb
PHM-dung2 <pp4992@naver.com>
2024-11-30 02:46:28 +0900

    중간과제10 해결

--- day15/2_중간과제10.js
+++ day15/2_중간과제10.js
@@ -4,57 +4,87 @@ let scoreArray = [ '1,90,95,2024','1,80,85,2023','2,70,50,2024']
 function inputFunc1(){
     let pName = document.querySelector('.pName').value;
     let pBirth = document.querySelector('.pBirth').value;
-
-    for(i=0 ; i <= personArray.length ; i++){
-        personArray.push = `${i+1},${pName},${pBirth}`;
-    } // for end
+    
+    personArray.push( `${personArray.length+1},${pName},${pBirth}` );
+    
     console.log(personArray);
    
 } // f end
-
 function inputFunc2(){
-    let pName = document.querySelector('.pName');
-    let pBirth = document.querySelector('.pBirth');
+    let pName = document.querySelector('.pName').value;
+    let pBirth = document.querySelector('.pBirth').value;
     let upScore = document.querySelector('.upScore').value;
     let downScore = document.querySelector('.downScore').value;
     let pYear = document.querySelector('.pYear').value;
 
-    for(i=0 ; i < personArray.length ; i++){
-        let pBoard = personArray[i]
+    let pId = '';
+    for(let j=0 ; j < personArray.length ; j++){
+        let pBoard = personArray[j];
         let pNum = pBoard.split(",");
-        console.log( pNum[1] )
-        if( pName == pNum[1] && pBirth == pNum[2] ){
-            scoreArray.splice( i , 0 , `${pNum(0)},${upScore},${downScore},${pYear}` );
-            ; console.log(scoreArray)
-        } // if end
-        else{ 
-            scoreArray.push( `${i+1},${upScore},${downScore},${pYear}` );
-        } // else end
+        if(pNum[1] == pName && pNum[2] == pBirth){pId = pNum[0];}
+    } // for2 end
+
+    let searchId = false;
+    for(let i=0 ; i < scoreArray.length ; i++){
+        let sBoard = scoreArray[i]
+        let sNum = sBoard.split(",");
+
+        if( sNum[0] == pId ){ 
+            scoreArray.splice( i+1 , 0 , `${pId},${upScore},${downScore},${pYear}` );
+            searchId = true;
+            break;
+        }
+    
+    } // for1 end
+    
+    if( searchId == false){
+        scoreArray.push(`${pId},${upScore},${downScore},${pYear}`);
+    }
+    
+    console.log(scoreArray);
+    
+} // f end
+
+function printFunc2(){
+    let html = '';
+    for( let i = 0 ; i < personArray.length ; i++){
+        let pBoard = personArray[i];
+        let pNum = pBoard.split(",");
+        let fullArray = []
+        for( let j = 0 ; j < scoreArray.length ; j++){
+            let sBoard = scoreArray[j];
+            let sNum = sBoard.split(",");
+            if( pNum[0] == sNum[0] ){ 
+                fullArray.push( `${pNum[1]},${pNum[2]},${sNum[1]},${sNum[2]},${sNum[3]}` );
+                console.log(fullArray); }// if end
+        } // for2 end
+            
+        for( let k = 0 ; k < fullArray.length ; k++){
+            let fBoard = fullArray[k];
+            let fNum = fBoard.split(",");
+            if(k == 0){ html += `  <tr>
+                                        <td>${fNum[0]}</td>
+                                        <td>${fNum[1]}</td>
+                                        <td>${fNum[2]}</td>
+                                        <td>${fNum[3]}</td>
+                                        <td>${fNum[4]}</td>
+                                    </tr>`
+            } else{ html += `  <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>${fNum[2]}</td>
+                                    <td>${fNum[3]}</td>
+                                    <td>${fNum[4]}</td>
+                                </tr>`
+
+            }
+            
+        } // for3 end
         
-    } // for end
-    console.log(scoreArray)
-}
-
-// let inputyear
-// console.log( nowYear )
-
-
- // for( i = 0 ; i < personArray.length ; i++ ){
-    //     let board1 = personArray(i);
-    //     let board2 = scoreArray(i);
-    //     let pArray = board1.split(",");
-    //     let sArray = board2.split(",");
-    //     console.log(pArray)
-    //     console.log(sArray)
-
-    //     if( pName == pArray[i] && pBirth == pArray[i+1] ){
-    //         personArray.splice( i , 0 , `'',''`)
-    //         personArray.splice( i , 0 , `${upScore},${downScore}`)
-    //     }
-    //     else{
-    //         personArray.splice( i , 0 , `${pName},${pBirth}`)
-    //         personArray.splice( i , 0 , `${upScore},${downScore}`)
-    //     }
-    //     console.log( personArray )
-    //     console.log( scoreArray )
-    // } // for end
+        console.log(fullArray);       
+        
+    } // for1 end
+    
+    document.querySelector('.personTable').innerHTML = html;
+    
+} // f end
