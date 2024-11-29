b09cc48c1520219fc6e296db3b0ba16475f44600
PHM-dung2 <pp4992@naver.com>
2024-11-29 10:36:04 +0900

    day15

--- day14/1_중간과제8.js
+++ day14/1_중간과제8.js
@@ -125,23 +125,40 @@ function postTitleInput( i ){ // i : 매개변수 , 상세 출력할 배열의
     document.querySelector('.contentBox').innerHTML = info[1];
     document.querySelector('.dayBox').innerHTML = info[3];
     document.querySelector('.viewBox').innerHTML = info[4];
-    document.querySelector('.btnBox'),innerHTML = 
+    document.querySelector('.btnBox').innerHTML = 
             `<button type="button" onclick="postDeleteFunc(${i})">삭제</button>
-            <button type="button">수정</button>`
+            <button type="button" onclick="postEditFunc(${i})">수정</button>`
 }
 
 // [4] 삭제함수 , 실행조건 : [삭제]버튼 클릭시
 function postDeleteFunc( i ){   
     console.log('삭제함수 실행')
     // 1. 배열내 특정한 인덱스 의 요소 제거 , 배열변수명.splice( 삭제할 인덱스 , 개수 )
+    let board = postArray[i]
+    let info = board.split(',');
     let input = prompt('비밀번호 입력');
-    if( input == info[2]){ postArray.splice( i , 1 ) }
+    if( input == info[2]){ postArray.splice( i , 1 ) };
     // 2. 화면 새로고침 / 다시 출력 / 다시ㅣ 함수 호출
-    
-    
+    postListFunc();
+    document.querySelector('.tilteBox').innerHTML = "";
+    document.querySelector('.contentBox').innerHTML = "";
+    document.querySelector('.dayBox').innerHTML = "";
+    document.querySelector('.viewBox').innerHTML = "";
 } // f end
 
 // [5] 수정함수 , 실행조건 : [수정]버튼 클릭시
-function postEditFunc(){   console.log('등록함수 실행')
-    
+function postEditFunc( i ){ console.log('등록함수 실행')
+    console.log('수정함수 실행')
+    // 1. 배열내 특정한 인덱스 의 요소 제거 , 배열변수명.splice( 삭제할 인덱스 , 개수 )
+    let board = postArray[i];
+    let info = board.split(',');
+    let input = prompt('비밀번호 입력');
+    if( input == info[2]){ 
+        info[0] = prompt('수정할 제목 입력');
+        info[1] = prompt('수정할 내용 입력');
+        postArray[i]=`${info[0]}, ${info[1]}, ${info[2]}, ${info[3]}, ${info[4]} `
+    }
+    // 2. 화면 새로고침 / 다시 출력 / 다시ㅣ 함수 호출
+    postListFunc();
+    postTitleInput( i );
 } // f end ,
