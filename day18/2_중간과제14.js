3463a4e9decf31eeae08c76ac050bc99e1bc58dc
PHM-dung2 <pp4992@naver.com>
2024-12-05 06:26:36 +0900

    css추가

--- day18/2_중간과제14.js
+++ day18/2_중간과제14.js
@@ -116,13 +116,15 @@ function dltFunc1(index){
 
 // 재고
 function 도서재고연결( 도서코드 ){
-    document.querySelector('.inputNum').innerHTML = 
+    document.querySelector('.btn2').innerHTML = 
             `<h4> 도서코드 ${ 도서코드 } : 재고목록 </h4>
-            <input class="num1" type="text" placeholder="재고량"/>
-            <input class="num2" type="text" placeholder="상태"/>
-            <input class="num3" type="text" placeholder="정가"/>
-            <input class="num4" type="date" placeholder="입고날짜"/>
-            <button onclick="재고등록함수(${ 도서코드 })" type="button">재고등록</button>`
+            <div>
+                <input class="num1" type="text" placeholder="재고량"/>
+                <input class="num2" type="text" placeholder="상태"/>
+                <input class="num3" type="text" placeholder="정가"/>
+                <input class="num4" type="date" placeholder="입고날짜"/>
+                <button onclick="재고등록함수(${ 도서코드 })" type="button">재고등록</button>
+            </div>`
             
 } // f end
 
